dd958ff1c7b5c6dd17547275ae672e72f2bc7886
marcrleonard <marc.r.leonard@gmail.com>
2018-10-13 12:27:34 -0600

    add in showcase

--- generic_functions.js
+++ generic_functions.js
@@ -57,10 +57,24 @@ window.onload = function () {
 
 
 var fullwidth = function (element) {
-    var ele = document.getElementById('showcaseProject');
-    ele.classList.add('expand');
+    // this functin runs when a project is clicked
+    var project_idx = element.getAttribute('project-num');
 
-    console.log(getPos(ele));
+    var full_project = all_projects[project_idx];
+    console.log(full_project);
+
+    var title_ele = document.getElementById('showcaseTitle');
+    var text_ele = document.getElementById('showcaseText');
+    var media_ele = document.getElementById('showcaseMedia');
+
+    title_ele.innerHTML = full_project.name;
+    text_ele.innerHTML = full_project.full_desc;
+    media_ele.innerHTML = full_project.bg_img;
+    // ele.classList.add('showcaseExpand');
+
+    // console.log(getPos(ele));
+
+
 
 
 
@@ -79,12 +93,16 @@ window.addEventListener('scroll', function (e) {
 
     this.console.log(elemRect);
 
+    var top = this.document.getElementById('navContainer');
+
     if (elemRect < window_offset) {
-        nav.classList.add("sticky")
+        nav.classList.add("sticky");
+        top.style.height = '50px'
     }
     else {
         // if ((nav.classList).includes('sticky')) {
-        nav.classList.remove("sticky")
+        nav.classList.remove("sticky");
+        top.style.height = '0px'
     };
 
 
@@ -109,38 +127,77 @@ var all_projects = [
     {
         'name': 'PixelStrings',
         'tags': ['PaaS'],
-        'desc': 'QD'
+        'short_desc': 'QD',
+        'bg_img': '',
+        'full_desc': `Currently, I am a core backend developer of PixelStrings,
+        a platform by <a class='whiteLink' href="http://cinnafilm.com">Cinnafilm</a> . <a class='whiteLink' href="http://pixelstrings.com"> PixelStrings</a>
+            specializes cloud video processing and delivery.
+        My job is two fold - automating the allocation and orcehstration cloud compute resources for
+        processing and delivery, and creating APIs to enable different type of
+        video conversion formats.
+        These services are accesed by the front end UI, as well as our public API.`,
+        'technologies': ['python'],
+        'full_media': '',
+        'links': ['http://pixelstrings.com']
     },
     {
         'name': 'ArtStream',
         'tags': ['SaaS'],
-        'desc': 'QD'
+        'short_desc': 'QD',
+        'bg_img': '',
+        'full_desc': `ArtStream is a patform for artists to showcase their work via the ArtStream channel on Roku. I created the
+        infrastructure and APIs to take a piece of artwork a user uploaded, and delivery it in video form with 'Ken Burns' style animations applied.`,
+        'technologies': ['python', 'javascript', 'MySQL', 'ImageMagik', 'Digital Ocean', 'PIL', 'FFMPEG'],
+        'full_media': '',
+        'links': ['https://www.artstreamvideos.com/']
     },
     {
         'name': 'ImageToVideo',
         'tags': ['Library'],
-        'desc': 'QD'
+        'short_desc': 'QD',
+        'bg_img': '',
+        'full_desc': `I created this to make a batteries included way to make an image into a video. ImageToVideo uses OpenCV/Numpy as a 'middle man'. It handles any zoom, resize, rotate functions (and eventually will power the plugin extensibility). All encoding is done via FFMPEG. In the future, I will be adding other imaging libraries. I have preliminary versions of using Pillow and Pyvips.`,
+        'technologies': ['FFMPEG', 'PIL', 'Numpy', 'OpenCV'],
+        'full_media': '',
+        'links': ['https://github.com/marcrleonard/ImageToVideo']
     },
     {
         'name': 'Photobooth',
         'tags': ['Application'],
-        'desc': 'QD'
+        'short_desc': 'QD',
+        'bg_img': '',
+        'full_desc': 'I created this app to use at my wedding. It was run on a Raspberry Pi, computer screen and a Canon 60d. Resulting photobooth image "strips" are texted to the subject!',
+        'technologies': ['RaspberryPi', 'Twilio', 'TKinter', 'FFMPEG', 'ImageMagik', 'python'],
+        'full_media': '',
+        'links': ['https://github.com/marcrleonard/Photobooth'],
     },
     {
         'name': 'Topo Plotter',
         'tags': ['Art'],
-        'desc': 'QD'
+        'short_desc': 'QD',
+        'bg_img': '',
+        'full_desc': '',
+        'technologies': '',
+        'full_media': '',
+        'links': [],
     },
     {
         'name': 'SimpleTimeOffset',
         'tags': ['Library', 'WIP'],
-        'desc': 'QD'
+        'short_desc': 'QD',
+        'bg_img': '',
+        'full_desc': `
+        This library provides a very simple way to add hour offsets to a given input time. While doing an iot project with micro python, I found that an internet connected device will get the correct time, but micropython would not give you the ability to add a timezone. So if you know (or figure out) the proper hourly offset, you can use this library to get the correct time. It has no dependencies (except the new_time method, so you can easily use in micro python. This does not do any other 'fancy' calculations like daylight savings time.`,
+        'technologies': ['micropython', 'python', 'esp8266'],
+        'full_media': '',
+        'links': ['https://github.com/marcrleonard/SimpleTimeOffset'],
     },
 ]
 
 
 var project_location = document.getElementById('projectBoxes')
-for (let project of all_projects) {
+for (let [idx, project] of all_projects.entries()) {
+    console.log(idx)
     var div = document.createElement('div');
 
     var center = document.createElement('center');
@@ -149,8 +206,9 @@ for (let project of all_projects) {
     var tags = document.createElement('h4');
     tags.innerHTML = project.tags;
     var desc = document.createElement('h5');
-    desc.innerHTML = project.desc
+    desc.innerHTML = project.short_desc;
 
+    div.setAttribute('project-num', idx);
     div.classList.add('projectBox');
     div.id = project.name;
 
