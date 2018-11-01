8c4c93ca2705d91094b9e57b383ccaa52844f4fb
marcrleonard <marc.r.leonard@gmail.com>
2018-11-01 07:50:47 -0600

    add build folder

---
+++ build/precache-manifest.0a366d471a97a9977578b1cf02f7c53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c0991c8df8a48326aae",
+    "url": "/static/js/main.0c0991c8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "0c0991c8df8a48326aae",
+    "url": "/static/css/main.1e69c6ab.chunk.css"
+  },
+  {
+    "revision": "edb3df1aadeab7ad85218c53d0c210c4",
+    "url": "/index.html"
+  }
+];
