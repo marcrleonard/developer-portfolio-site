a3335f6389298e2f0b188e67ac130fd9e432fb9f
marcrleonard <marc.r.leonard@gmail.com>
2020-01-24 06:53:57 -0700

    add build folder back, rename to docs for github pages test

---
+++ docs/precache-manifest.8e9b13b757fb5365f8a762f34dab28f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1d085bb5043e8864e6f",
+    "url": "/static/js/main.d1d085bb.chunk.js"
+  },
+  {
+    "revision": "118809d600a566e42ccf",
+    "url": "/static/js/1.118809d6.chunk.js"
+  },
+  {
+    "revision": "d1d085bb5043e8864e6f",
+    "url": "/static/css/main.60be4536.chunk.css"
+  },
+  {
+    "revision": "d49aba8631fd5f573fe72222dd8d5f5d",
+    "url": "/index.html"
+  }
+];
