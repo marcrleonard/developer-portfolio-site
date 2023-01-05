f1b28fcd04529a073ecef8995a923c60f9974ddd
marcrleonard <marc.r.leonard@gmail.com>
2023-01-05 11:53:43 -0700

    fix styling

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ NEWS_ITEMS = [
 	# {
 	# 	'thumbnail': 'img/thumbs/4-3.jpg',
 	# 	'full_image': 'img/news/1.jpg',
-	# 	'title': 'Building brands through customer service',
-	# 	'date': 'August 9, 2021',
-	# 	'category': 'Branding',
+	# 	'title': 'Django Is Awesome.',
+	# 	'date': 'January 3, 2022',
+	# 	'category': 'Python',
 	# 	'text': [
-	# 		'BuckyCavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.',
-	# 		"In today’s digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user’s first impression relates to web design. It’s also why web design services can have an immense impact on your company’s bottom line.",
-	# 		"That’s why more companies are not only reevaluating their website’s design but also partnering with Kura, the web design agency that’s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business.",
+	# 		"Django is awesome.",
+	# 		"That is all."
 	# 	],
 	# }
 ]
